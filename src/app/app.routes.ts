0f538af9941d9be54c2d3d3df492bdf931b41420
tuan anh <dta89uct@gmail.com>
2024-01-19 11:49:35 +0700

    add live folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import {Routes} from '@angular/router';
 
-import {adminGuard} from "./guard/admin-guard.guard";
 import {loginGuard} from "./guard/login.guard";
 import {MainComponent} from "./components/main/main.component";
 import {RegisterComponent} from "./components/register/register.component";
 import {LoginComponent} from "./components/login/login.component";
 import {ForgotPasswordComponent} from "./components/forgot-password/forgot-password.component";
+import {MainIntroComponent} from "./components/main/main-intro/main-intro.component";
 
 
 export const routes: Routes = [
-  {path: '', component: MainComponent},
-  {path:'register',component:RegisterComponent,canActivate:[loginGuard]},
-  {path: "login", component: LoginComponent,canActivate:[loginGuard]},
-  {path: "forgot", component: ForgotPasswordComponent,canActivate:[loginGuard]},
+  {
+    path: '', component: MainComponent,
+    children: [
+      {path:'',component:MainIntroComponent}
+    ]
+  },
+  {path: 'register', component: RegisterComponent, canActivate: [loginGuard]},
+  {path: "login", component: LoginComponent, canActivate: [loginGuard]},
+  {path: "forgot", component: ForgotPasswordComponent, canActivate: [loginGuard]},
   // {
   //   path: "admin",canActivateChild:[adminGuard],
   //   children:[{path:""}]
